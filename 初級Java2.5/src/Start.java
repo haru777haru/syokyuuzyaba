df8e34de7af889dc65b394606d7c5a100ac7aada
sigaharu <zeal23310007@fsgcl.jp>
2024-05-14 12:04:50 +0900

    初級2.5

---
+++ 初級Java2.5/src/Start.java
@@ -0,0 +1,17 @@
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+	
+      int x=2;
+      int y=3;
+      System.out.println(x*y);
+      MyFrame frame1=new RectFrame();
+      frame1.setLocation(50,300);
+      MyFrame frame2=new RectFrame();
+      frame2.setLocation(80,300);
+		
+	}
+
+}
